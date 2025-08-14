bcc032f29ec90c1abf29003e7d252d665da8091e
binspr0ut <plattenecho@gmail.com>
2025-08-14 11:01:04 +0700

    Initial Commit

---
+++ SwiftCafe/SwiftCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCafeApp.swift
+//  SwiftCafe
+//
+//  Created by Mahardika Putra Wardhana on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftCafeTests/SwiftCafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCafeTests.swift
+//  SwiftCafeTests
+//
+//  Created by Mahardika Putra Wardhana on 14/08/25.
+//
+
+import Testing
+@testable import SwiftCafe
+
+struct SwiftCafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
